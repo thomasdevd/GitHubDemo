e25018c71584754be3c6a78b6d4de582d36be2c8
thomas.deva <thomas.deva@QT-INBG-LT0414.QUALITEST.LOCAL>
2022-01-31 15:54:15 +0530

    Second Commit

--- GitHubDemo/src/test/java/com/test/SampleTest.java
+++ GitHubDemo/src/test/java/com/test/SampleTest.java
@@ -8,6 +8,7 @@ public class SampleTest {
 	public void login() {
 		System.out.println("This is login method");
 		System.out.println("This is new Comment added");
+		System.out.println("This is second new Comment added");
 	}
 
 }
