0970bd7271b1a52266c88354189d035c0dcfdd3f
thomas.deva <thomas.deva@QT-INBG-LT0414.QUALITEST.LOCAL>
2022-01-31 14:39:49 +0530

    First commit

---
+++ GitHubDemo/src/test/java/com/test/SampleTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+
+public class SampleTest {
+
+	public void login() {
+		System.out.println("This is login method");
+		System.out.println("This is new Comment added");
+	}
+
+}
